cdd226ba855176c8fbddd3eb684445767be0c0bd
quanlv99 <quanlv.fw@gmail.com>
2023-01-03 17:32:50 +0700

    change verify method

--- utils/verify.ts
+++ utils/verify.ts
@@ -5,7 +5,7 @@ const verify = async (contractAddress: string, args: any[]) => {
     try {
         await run("verify:verify", {
             address: contractAddress,
-            erc721IArguments: args,
+            constructorArguments: args,
         })
     } catch (e: any) {
         if (e.message.toLowerCase().includes("already verified")) {
