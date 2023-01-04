c36813858df6d4ab56f46f3a936b2f250f41c385
quanlv99 <quanlv.fw@gmail.com>
2023-01-04 10:24:01 +0700

    ignore deployments folder & edit some comments

--- arguments.ts
+++ arguments.ts
@@ -6,5 +6,3 @@ module.exports = [
     0,
     "0x0000000000000000000000000000000000000000",
 ]
-
-// hh verify --constructor-args arguments.ts 0x50aDf55E432BD10B7a1e2820c04DE09faA183869 --network goerli
